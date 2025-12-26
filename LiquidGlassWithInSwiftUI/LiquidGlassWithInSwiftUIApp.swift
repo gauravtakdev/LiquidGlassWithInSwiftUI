9d59f568bbd2b1eec378f445afd3c6672b5dc230
Gaurav Tak <gaurav.tak@roro.io>
2025-12-26 10:27:29 +0530

    first commit

---
+++ LiquidGlassWithInSwiftUI/LiquidGlassWithInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassWithInSwiftUIApp.swift
+//  LiquidGlassWithInSwiftUI
+//
+//  Created by Gaurav Tak on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassWithInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
